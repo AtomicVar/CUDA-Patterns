b43fe8fd861e992a514df26440e0f7bcec2d683d
AtomicVar <gs0801@foxmail.com>
2023-11-10 15:11:57 +0800

    Update histogram_interleaved.cuh

--- 4-histogram/histogram_interleaved.cuh
+++ 4-histogram/histogram_interleaved.cuh
@@ -1,5 +1,6 @@
 #pragma once
 
+// 实际上就是 Grid-Stride Looping
 __global__ void histogram_interleaved(int* data, int* histogram, int len, int num_bins) {
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
     int total_threads = gridDim.x * blockDim.x;
